7ceb356f22cd263c97e869215342b29bb06c48a4
ProwainK <38719326+ProwainK@users.noreply.github.com>
2025-06-28 19:11:52 +0800

    Add files via upload

---
+++ renameDB.js
@@ -0,0 +1,11 @@
+window.renameDB = {
+
+  "Synchronica (シンクロニカ)": "Synchronica",
+
+  "BONUS GAME 1 - Donkey Kong Jungle Beat": "Donkey Kong Jungle Beat",
+  "BONUS GAME 2 - Super Mario Odyssey": "Super Mario Odyssey",
+  "BONUS GAME 3 - Bloodborne": "Bloodborne",
+  "BONUS GAME 4 - Mario Kart World": "Mario Kart World",
+
+  "BONUS GAME": "BONUS GAME"
+}
