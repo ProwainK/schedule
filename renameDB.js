de7b3eb55034b28ac51b313aeffe0f286d188964
ProwainK <38719326+ProwainK@users.noreply.github.com>
2025-07-05 17:37:31 +0800

    Add files via upload

--- renameDB.js
+++ renameDB.js
@@ -1,6 +1,6 @@
 window.renameDB = {
 
-  "RiJ": {},
+  "RTA in Japan Summer 2025": {},
 
   "悪魔城ドラキュラXX": "Castlevania: Dracula X",
   "古明地さとりの情操教育": "Komeiji Satori no Jousou Kyouiku",
@@ -11,31 +11,73 @@ window.renameDB = {
   "新・熱血硬派 くにおたちの挽歌 -with River City Girls Extra-": "River City Girls Zero",
   "姐": "ane-san",
 
+  "アーネスト・エバンス": "Earnest Evans",
+  "いただきストリート　ゴージャスキング": "Itadaki Street: Gorgeous King",
+  "METAL MAX2": "METAL MAX 2",
+  "北海道4,000km 道内全市町村踏破": "Visit all municipalities in Hokkaido",
+  "アメリカンバトルドーム": "American Battle Dome",
+  "カラスのおしごと": "Just Crow Things",
+  "サカつく2002 J.LEAGUEプロサッカークラブをつくろう!": "Sakatsuku 2002: J.League Pro Soccer Club wo Tsukurou!",
+  "ウルトラ怪獣モンスターファーム": "Ultra Kaiju Monster Rancher",
+  "アメリカ横断ウルトラクイズ": "America Oudan Ultra Quiz: Shijou Saidai no Tatakai",
+  "メタルギアソリッド インテグラル": "Metal Gear Solid: Integral",
 
-
-
-
-
-
-
-
-
+  "ときめきメモリアル Girl's Side 4th Heart": "Tokimeki Memorial Girl's Side 4th Heart",
+  "ルミネス リマスター": "Lumines Remastered",
+  "救国のスネジンカ:Sentinel Girls2": "Snezhinka: Sentinel Girls 2",
+  "ロックマン9 野望の復活!!": "Mega Man 9",
+  "ロックマンDASH 鋼の冒険心": "Mega Man Legends",
+  "東方永夜抄 〜 Imperishable Night.": "Touhou Eiyashou ~ Imperishable Night.",
+  "アフターイメージ": "Afterimage",
+  "NieR:Automata": "NieR: Automata",
   "Barbie Super Model": "Barbie: Super Model",
-  "BIOHAZARD RE:2": "Resident Evil 2 (2019)",
-  "Cook,serve,Delicious!3?!": "Cook, Serve, Delicious! 3?!",
+  "デジモンワールド": "Digimon World",
+  "青鬼(2024)": "Ao Oni (2024)",
 
+  "BIOHAZARD RE:2": "Resident Evil 2 (2019)",
+  "モンスターハンター2（ドス）": "Monster Hunter 2",
+  "新鬼武者": "Onimusha: Dawn of Dreams",
+  "noita": "Noita",
+  "松方弘樹のスーパートローリング": "Matsukata Hiroki no Super Trawling",
+  "パウ・パトロール ザ・ムービー：アドベンチャーシティがよんでいる": "PAW Patrol The Movie: Adventure City Calls",
+  "カンパニーマン 社畜の下剋上": "The Company Man",
+  "アリス・エスケイプト！": "Alice Escaped!",
+  "ドラゴンクエストIV 導かれし者たち": "Dragon Quest IV: Chapters of the Chosen",
+  "ドラゴンクエストモンスターズ キャラバンハート": "Dragon Quest Monsters: Caravan Heart",
 
   "真・女神転生if...": "Shin Megami Tensei if...",
-  "世界樹の迷宮II 諸王の聖杯 HD REMASTER": "Etrian Odyssey II HD",
+  "クロノアーク": "Chrono Ark",
+  "ZWEI2(ツヴァイ2)": "Zwei: The Ilvard Insurrection",
+  "イースⅠクロニクルズ": "Ys Chronicles 1",
+  "機動戦士ガンダム 一年戦争": "Mobile Suit Gundam: One Year War",
+  "SCHiM - スキム -": "SCHiM",
+  "ロックマンエグゼ5": "Mega Man Battle Network 5",
   "聖剣伝説3": "Trials of Mana (2020)",
+
+  "アーマード・コア ナインブレイカー": "Armored Core: Nine Breaker",
+  "wattam": "Wattam",
+  "ふしぎの城のヘレン+": "Helen's Mysterious Castle",
+  "レスキューショット ブービーぼー": "Rescue Shot",
+  "スーパーワギャンランド": "Super Wagyan Land",
+  "TETRIS THE GRAND MASTER 4 -ABSOLUTE EYE-": "TETRIS THE GRAND MASTER 4 - ABSOLUTE EYE -",
+  "フィリスのアトリエ ～不思議な旅の錬金術士～ DX": "Atelier Firis: The Alchemist and the Mysterious Journey DX",
+  "アーケードアーカイブス トリオ・ザ・パンチ": "Trio the Punch: Never Forget Me",
+  "スゴイツヨイトウフ": "The Strongest TOFU",
+  "ゲームセンターCX 有野の挑戦状 1+2 REPLAY": "Game Center CX: Arino no Chousenjou 1 + 2 Replay",
+
+  "ドラゴンファングZ": "DragonFangZ - The Rose & Dungeon of Time",
+  "DRAG-ON DRAGOON 3": "Drakengard 3",
+  "世界樹の迷宮II 諸王の聖杯 HD REMASTER": "Etrian Odyssey II HD",
+  "ブリーチ ヒート・ザ・ソウル7": "Bleach: Heat the Soul 7",
+  "ゴジラ怪獣大決戦": "Godzilla: Monster War",
+  "ケツバトラー": "Ketsu Battler",
   "電車でGO!": "Densha de Go!",
-  "青鬼(2024)": "Ao Oni (2024)",
-  "北海道4,000km 道内全市町村踏破": "Visit all municipalities in Hokkaido",
-
-  "みんな大好き塊魂アンコール＋王様プチメモリー": "We Love Katamari REROLL+ Royal Reverie",
+  "サルゲッチュ": "Ape Escape",
+  "迷宮組曲 ミロンの大冒険": "Milon's Secret Castle",
+  "Cook,serve,Delicious!3?!": "Cook, Serve, Delicious! 3?!",
   "ロマンシング サガ3 HDリマスター": "Romancing SaGa 3 (HD Remaster)",
-  "魔界戦記ディスガイア6": "Disgaea 6: Defiance of Destiny",
 
+  "魔界戦記ディスガイア6": "Disgaea 6: Defiance of Destiny",
 
   "GDQ": {},
 
