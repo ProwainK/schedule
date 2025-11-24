f7b2ea6b54ec3c7b61e1d1f10577e128229b3935
ProwainK <38719326+ProwainK@users.noreply.github.com>
2025-11-24 12:09:34 +0800

    Add files via upload

--- renameDB.js
+++ renameDB.js
@@ -11,63 +11,6 @@
 
     "RTA in Japan Winter 2025": {},
 
-    "星のカービィ Wii デラックス": "Kirby's Return to Dream Land Deluxe",
-    "GS美神 ～除霊師はナイスバディ～": "Ghost Sweeper Mikami: Joreishi wa Nice Body",
-    "ゴミ箱 -GOMIBAKO-": "Trash Panic",
-    "ポケモンでパネポン": "Pokémon Puzzle Challenge",
-    "アケアカNEOGEO パズルボブル2": "Puzzle Bobble 2",
-    "月光妖怪": "Moonlight Ghost",
-    "東方紅魔郷 〜 the Embodiment of Scarlet Devil.": "Touhou Koumakyou ~ the Embodiment of Scarlet Devil",
-    "PUNCH-OUT!!": "PUNCH-OUT!! (2009)",
-    "世界樹の迷宮Ⅳ 伝承の巨神": "Etrian Odyssey IV: Legends of the Titan",
-
-    "リトルタウンヒーロー": "Little Town Hero",
-    "桃太郎電鉄ワールド〜地球は希望でまわってる！〜": "Momotaro Dentetsu World: Chikyuu ha Kibou de Mawatteru!",
-    "BORDER BREAK(PS4)": "Border Break",
-    "精霊機フレイリート": "Divine Dynamo Flamefrit",
-    "スターフォックス64": "Star Fox 64",
-    "BLADE CHIMERA - ブレードキメラ": "BLADE CHIMERA",
-    "マリオカート8": "Mario Kart 8",
-    "首都高バトル": "Shutokou Battle",
-    "モンスターハンターワイルズ": "Monster Hunter Wilds",
-
-    "オウガバトル64 Person of Lordly Caliber": "Ogre Battle 64: Person of Lordly Caliber",
-    "シャドウジェネレーションズ": "Shadow Generations",
-    "グレゴリーホラーショー ソウルコレクター": "Gregory Horror Show",
-    "マリオゴルフGB": "Mario Golf (Game Boy Color)",
-    "Refind Self: 性格診断ゲーム": "Refind Self: The Personality Test Game",
-    "マリオストーリー": "Paper Mario",
-    "エイリアンVSプレデター": "Alien Vs Predator",
-    "キャッスルヴァニア": "Castlevania: Lament of Innocence",
-    "小鳩ヶ丘高校女子ぐろ〜部 Gleam of Force": "Glove on Fight 2: Gleam of Force",
-    "星のカービィ ウルトラスーパーデラックス": "Kirby Super Star Ultra",
-    "星のカービィ2": "Kirby's Dream Land 2",
-    "ゼルダ": "Zelda (Game & Watch)",
-    "ゼルダの伝説 時のオカリナ": "The Legend of Zelda: Ocarina of Time",
-    "サラダの国のトマト姫": "Princess Tomato in the Salad Kingdom",
-
-    "トマトアドベンチャー": "Tomato Adventure",
-    "不思議のダンジョン 風来のシレン6 とぐろ島探検録": "Shiren the Wanderer: The Mystery Dungeon of Serpentcoil Island",
-    "ドラゴンクエストキャラクターズ トルネコの大冒険2 不思議のダンジョン": "Torneko: The Last Hope",
-    "スペルトナエル": "Spell Tonaeru",
-    "カエルの為に鐘は鳴る": "Kaeru no Tame ni Kane wa Naru",
-    "ピクミン4": "Pikmin 4",
-    "スーパーマリオワールド": "Super Mario World",
-    "スーパーマリオメーカー 2": "Super Mario Maker 2",
-    "スーパーマリオギャラクシー": "Super Mario Galaxy",
-    "フィズとにじいろの星": "Fiz and the Rainbow Planet",
-    "朧村正 - 元禄怪奇譚": "Muramasa: The Demon Blade",
-
-
-
-
-
-
-
-
-
-    "スーパーメトロイド": "Super Metroid",
-
 
     "RTA in Japan Summer 2025": {},
 
